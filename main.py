e58a5dd988493c0636ae8e706119190fdb486e31
Tiffany <tgateley22@gmail.com>
2024-10-13 02:59:35 -0500

    Implement the pays method

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ class Bill:
     Object that contains data about a bill, such as the billing cycle
     and total amount of the bill.
     """
+
     def __init__(self, amount, bill_cycle):
         self.amount = amount
         self.bill_cycle = bill_cycle
@@ -14,12 +15,13 @@ class Roommate:
     share of the bill.
     """
 
-    def __init(self, name, days_in_house):
+    def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        pass
+    def pays(self, bill, roommate2):
+        weight = self.days_in_house / (self.days_in_house + roommate2.days_in_house)
+        return round(weight * bill.amount, 2)
 
 
 class PdfReport:
@@ -34,3 +36,9 @@ class PdfReport:
         pass
 
 
+the_bill = Bill(amount=120, bill_cycle="March 2024")
+john = Roommate(name='John', days_in_house=20)
+mary = Roommate(name='Mary', days_in_house=25)
+
+print("John pays: ", john.pays(bill=the_bill, roommate2=mary))
+print("Mary pays: ", mary.pays(bill=the_bill, roommate2=john))
