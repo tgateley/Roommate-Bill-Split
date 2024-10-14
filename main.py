8b8672eb6c5eda67ffb9103d1bc89cef98181b1b
Tiffany <tgateley22@gmail.com>
2024-10-14 02:50:37 -0500

    add webbrowser.open and add image to the pdf, and minor changes to the formatting of the pdf

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import fpdf
+import webbrowser
+
+from fpdf import FPDF
 
 
 class Bill:
@@ -36,25 +38,29 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, roommate1, roommate2, bill):
-        pdf = fpdf.FPDF(orientation='P', unit='mm', format='A4')
+        pdf = FPDF(orientation='P', unit='mm', format='A4')
         pdf.add_page()
+
+        # Add icon
+        pdf.image("house.png", w=20, h=20)
 
         # Title of the bill
         pdf.set_font(family='Times', size=24, style='B')
         pdf.cell(w=200, h=10, txt="Flatmates Bill", border=0, align='C', ln=1)
+        pdf.set_font(family='Times', size=14, style='B')
         pdf.cell(w=100, h=20, txt=f"Period: {bill.bill_cycle}", border=0, ln=2)
 
         # name and amount owed by the fist roommate
-        pdf.set_font(family='Times', size=20, style='B')
+        pdf.set_font(family='Times', size=14)
         pdf.cell(w=100, h=10, txt=roommate1.name, border=0)
         pdf.cell(w=100, h=10, txt=str(roommate1.pays(bill, roommate2)), border=0, ln=1)
 
         # Name and amount owed by the second roommate
-        pdf.set_font(family='Times', size=20, style='B')
         pdf.cell(w=100, h=10, txt=roommate2.name, border=0)
         pdf.cell(w=100, h=10, txt=str(roommate2.pays(bill, roommate1)), border=0, ln=1)
 
         pdf.output(self.filename)
+        webbrowser.open(self.filename)
 
 
 the_bill = Bill(amount=120, bill_cycle="March 2024")
