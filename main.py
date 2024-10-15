79b64178347a46d42ed516bfab12eec1ae3ee67e
Tiffany <tgateley22@gmail.com>
2024-10-15 01:43:53 -0500

    Implement input functions and add a bill total to the pdf

--- main.py
+++ main.py
@@ -59,15 +59,26 @@ class PdfReport:
         pdf.cell(w=100, h=10, txt=roommate2.name, border=0)
         pdf.cell(w=100, h=10, txt=str(roommate2.pays(bill, roommate1)), border=0, ln=1)
 
+        pdf.cell(w=100, h=10, txt="Bill total: ", border=0)
+        pdf.cell(w=100, h=10, txt=str(bill.amount), border=0, ln=1)
+
         pdf.output(self.filename)
         webbrowser.open(self.filename)
 
 
-the_bill = Bill(amount=120, bill_cycle="March 2024")
-john = Roommate(name='John', days_in_house=20)
-mary = Roommate(name='Mary', days_in_house=25)
+bill_amount = float(input("Enter the bill amount: "))
+bill_period = input("Ender the bill period: ")
+the_bill = Bill(amount=bill_amount, bill_cycle=bill_period)
 
-print("John pays: ", john.pays(bill=the_bill, roommate2=mary))
-print("Mary pays: ", mary.pays(bill=the_bill, roommate2=john))
+name1 = input("Enter your name: ")
+days_in_house1 = int(input(f"How many days did {name1} stay in home during the bill period? "))
+name2 = input("Enter your roommate's name: ")
+days_in_house2 = int(input(f"How many days did {name2} stay in home during the bill period? "))
+
+roommate1 = Roommate(name=name1, days_in_house=days_in_house1)
+roommate2 = Roommate(name=name2, days_in_house=days_in_house2)
+
+print(f"{name1}pays: ", roommate1.pays(bill=the_bill, roommate2=roommate2))
+print("Mary pays: ", roommate2.pays(bill=the_bill, roommate2=roommate1))
 Pdf = PdfReport(f"{the_bill.bill_cycle}.pdf")
-Pdf.generate(roommate1=john, roommate2=mary, bill=the_bill)
+Pdf.generate(roommate1, roommate2, the_bill)
