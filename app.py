fa9dc6a3e3996ee5aad896641f3e8639ce27eee1
Tiffany <tgateley22@gmail.com>
2024-10-29 08:29:11 -0500

    Implement the bill form page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, flash
 from flask.views import MethodView
-from wtforms import Form
+from wtforms import Form, StringField, SubmitField
 
 app = Flask(__name__)
 
@@ -12,7 +12,8 @@ class HomePage(MethodView):
 
 class BillFormPage(MethodView):
     def get(self):
-        return render_template('bill_form_page.html')
+        bill_form = BillForm()
+        return render_template('bill_form_page.html', bill_form=bill_form)
 
 
 class ResultsPage(MethodView):
@@ -20,11 +21,19 @@ class ResultsPage(MethodView):
 
 
 class BillForm(Form):
-    pass
+    amount = StringField(label="Bill Amount: ")
+    period = StringField(label="Bill-Cycle: ")
+    roommate1 = StringField(label="Name: ")
+    days_in_house1 = StringField(label="Days stayed in house: ")
+    roommate2 = StringField(label="Name: ")
+    days_in_house2 = StringField(label="Days stayed in the house: ")
+
+    button = SubmitField(label='Submit')
+
 
 
 app.add_url_rule("/", view_func=HomePage.as_view('home_page'))
 app.add_url_rule("/bill", view_func=BillFormPage.as_view('bill_form_page'))
 app.add_url_rule("/result", view_func=ResultsPage.as_view('results_page'))
 
-app.run()
+app.run(debug=True)
