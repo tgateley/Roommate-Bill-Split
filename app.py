35059082aaf5d9c071c39c8e43416ba0fe522796
Tiffany <tgateley22@gmail.com>
2024-11-12 04:36:52 -0600

    Implement the results page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
+from roommate_bill import roommates
 
 app = Flask(__name__)
 
@@ -18,18 +19,27 @@ class BillFormPage(MethodView):
 
 class ResultsPage(MethodView):
     def post(self):
-        billform = BillForm(request.form)
-        amount = billform.amount.data
-        return amount
+        bill_form = BillForm(request.form)
+
+        bill = roommates.Bill(float(bill_form.amount.data), bill_form.period.data)
+        roommate1 = roommates.Roommate(bill_form.roommate1.data, float(bill_form.days_in_house1.data))
+        roommate2 = roommates.Roommate(bill_form.roommate2.data, float(bill_form.days_in_house2.data))
+        return render_template("results.html",
+                               name1=roommate1.name,
+                               amount1=roommate1.pays(bill, roommate2),
+                               name2=roommate2.name,
+                               amount2=roommate2.pays(bill, roommate1))
 
 
 class BillForm(Form):
     amount = StringField(label="Bill Amount: ")
     period = StringField(label="Bill-Cycle: ")
+
     roommate1 = StringField(label="Name: ")
     days_in_house1 = StringField(label="Days stayed in house: ")
-    roommate2 = StringField(label="Name: ")
+
     days_in_house2 = StringField(label="Days stayed in the house: ")
+    roommate2 = StringField(label="Name: ")
 
     button = SubmitField(label='Submit')
 
